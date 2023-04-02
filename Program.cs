145fc915567723414437e744c6433b397bb05cd7
Fariddmusa <faridam@code.edu.az>
2023-04-02 21:49:27 +0400

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] number = { 3, -4, 56, 7, -3, -66, 5 };
+int positivecount = 0;
+int negativecount = 0;
+for(int i = 0; i < number.Length; i++)
+{
+    if (number[i] > 0)
+    {
+        positivecount++;
+    }
+    else if (number[i] < 0)
+    {
+        negativecount++;
+    }
+    
+}
+Console.WriteLine(positivecount);
+Console.WriteLine(negativecount);
