b063dc713bf541ed88b8c50ba5e8430b1dd490c9
Fariddmusa <faridam@code.edu.az>
2023-04-02 22:54:55 +0400

    added Program2.cs

---
+++ Program2.cs
@@ -0,0 +1,10 @@
+﻿int[] number = { 4, 35, 3, 5, 67, 8, 2 };
+int maxnumber = number[0];
+for(int i = 0; i < number.Length; i++)
+{
+    if (number[i] > maxnumber)
+    {
+        maxnumber = number[i];
+    }
+}
+Console.WriteLine(maxnumber);
